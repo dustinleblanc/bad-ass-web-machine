f4d0d93b6da3ddd48c32738928d41a38d689a7e4
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2017-06-20 16:43:19 -0400

    Add ampnewrelic

---
+++ web/modules/custom/ampnewrelic/src/EventSubscriber/AmpnewrelicSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\ampnewrelic\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * Class AmpnewrelicSubscriber.
+ *
+ * @package Drupal\ampnewrelic
+ */
+class AmpnewrelicSubscriber implements EventSubscriberInterface {
+
+
+  /**
+   * Constructs a new AmpnewrelicSubscriber object.
+   */
+  public function __construct() {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    $events['kernel.request'] = ['disableNewRelic'];
+
+    return $events;
+  }
+
+  /**
+   * This method is called whenever the kernel.request event is
+   * dispatched.
+   *
+   * @param GetResponseEvent $event
+   */
+  public function disableNewRelic(GetResponseEvent $event) {
+    if (extension_loaded('newrelic') && $this->isAmpRequest($event)) {
+      newrelic_disable_autorum();
+      newrelic_ignore_transaction();
+    }
+  }
+
+  private function isAmpRequest(GetResponseEvent $event) {
+    return $event->getRequest()->query->get('amp');
+  }
+
+}
